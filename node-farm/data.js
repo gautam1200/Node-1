678ff12aa0394a1189b2a4e9d5157253d33059cd
gautam501 <gautamparmar03214@gmail.com>
2025-07-11 15:29:10 +0530

    first commit

---
+++ node-farm/data.js
@@ -0,0 +1,26 @@
+let express = require("express")
+let fs = require("fs")
+
+let app = express()
+
+let datajson = fs.readFileSync('./data.json')
+let data = JSON.parse(datajson)
+
+app.set("view engine","ejs")
+
+app.get('/',function (req,res) {
+    res.render('data',{data})
+})
+
+app.get('/product/:id', function (req, res) {
+    let id = req.params.id 
+    let product = data[id]; 
+    res.render('product', { data: product });
+});
+
+app.get('/product',function (req , res){
+    res.render('product',{product})
+})
+
+
+app.listen(3333)
