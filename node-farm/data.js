60cf88e1e35c12b70cdb7b7777ae4a1009823257
Gautam Parmar <gautamparmar03214@gmail.com>
2025-07-14 10:50:56 +0530

    hello

--- node-farm/data.js
+++ node-farm/data.js
@@ -11,16 +11,16 @@ app.set("view engine","ejs")
 app.get('/',function (req,res) {
     res.render('data',{data})
 })
-
+  
 app.get('/product/:id', function (req, res) {
     let id = req.params.id 
     let product = data[id]; 
     res.render('product', { data: product });
 });
-
+  
 app.get('/product',function (req , res){
-    res.render('product',{product})
-})
-
-
+    res.render('product',{product})   
+})   
+     
+ 
 app.listen(3333)
